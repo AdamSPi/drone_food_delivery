732df2fa1e033701ba5292b4f1a2fd312d1e5e9d
Adam SP <adamsp123@gmail.com>
2019-04-20 20:37:45 -0400

    fixed unexpected async behavior #ihatejavascript

--- ar-drone.js
+++ ar-drone.js
@@ -25,71 +25,108 @@ client.on('navdata', function(navdata) {
     }
 });
 
-setInterval(
-    function() {
-        // The emergency: true option recovers your drone from emergency mode that can
-        // be caused by flipping it upside down or the drone crashing into something.
-        // In a real program you probably only want to send emergency: true for one
-        // second in the beginning, otherwise your drone may attempt to takeoff again
-        // after a crash.
-        control.ref({fly: true, emergency: true});
-        // This command makes sure your drone hovers in place and does not drift.
-        control.pcmd();
-        // This causes the actual udp message to be send (multiple commands are
-        // combined into one message)
-        control.flush();
-    }, 30);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function repeat(action, duration) {
+    var live = true;
+    setTimeout(function () {
+        live = false;
+        controller.pcmd();
+        controller.flush()
+    }, duration);
+
+    while (live) {
+        action();
+        await sleep(30);
+    }
+}
+
+// Take off
+async function takeoff() {
+    await repeat(
+        function() {
+            // The emergency: true option recovers your drone from emergency mode that can
+            // be caused by flipping it upside down or the drone crashing into something.
+            // In a real program you probably only want to send emergency: true for one
+            // second in the beginning, otherwise your drone may attempt to takeoff again
+            // after a crash.
+            control.ref({fly: true, emergency: true});
+            // This command makes sure your drone hovers in place and does not drift.
+            control.pcmd();
+            // This causes the actual udp message to be send (multiple commands are
+            // combined into one message)
+            control.flush();
+        }, 3000);
+    await stabilize(1500);
+}
 
 function navdata_to_speed(val) {
     // (-2) pitch -> front(.5)
     return val/(-4);
 };
 
-function stabilize(controller, duration) {
-    setInterval(function() {
-        maneuver = { front: -navdata_to_speed(pitch),
-                     left: -navdata_to_speed(roll) };
-        controller.pcmd(maneuver);
-        controller.flush();
-    }, duration);
-    setInterval(function() {
-        controller.pcmd();
-        controller.flush()
-    }, 30);
+async function stabilize(duration, controller = control) {
+    await repeat(
+        function() {
+            maneuver = { front: -navdata_to_speed(pitch),
+                         left: -navdata_to_speed(roll) };
+            controller.pcmd(maneuver);
+            controller.flush();
+        }, duration);
 }
 
-function move_forward(controller, duration){
-    setInterval(function() {
-        maneuver = { front: .5,
-                     left: -navdata_to_speed(roll) };
-        controller.pcmd(maneuver);
-        controller.flush();
-    }, duration);
+async function move_forward(duration, controller = control){
+    await repeat(
+        function() {
+            maneuver = { front: .5,
+                         left: -navdata_to_speed(roll) };
+            controller.pcmd(maneuver);
+            controller.flush();
+        }, duration);
+    await stabilize(1500);
 }
 
-function move_backward(controller, duration){
-    setInterval(function() {
-        maneuver = { back: .5,
-                     left: -navdata_to_speed(roll) };
-        controller.pcmd(maneuver);
-        controller.flush();
-    }, duration);
+async function move_backward(duration, controller = control){
+    await repeat(
+        function() {
+            maneuver = { back: .5,
+                         left: -navdata_to_speed(roll) };
+            controller.pcmd(maneuver);
+            controller.flush();
+        }, duration);
+    await stabilize(1500);
 }
 
-function move_left(controller, duration){
-    setInterval(function() {
-        maneuver = { left: .5,
-                     front: -navdata_to_speed(pitch) };
-        controller.pcmd(maneuver);
-        controller.flush();
-    }, duration);
+async function move_left(duration, controller = control){
+    await repeat(
+        function() {
+            maneuver = { left: .5,
+                         front: -navdata_to_speed(pitch) };
+            controller.pcmd(maneuver);
+            controller.flush();
+        }, duration);
+    await stabilize(1500);
 }
 
-function move_right(controller, duration){
-    setInterval(function() {
-        maneuver = { right: .5,
-                     front: -navdata_to_speed(pitch) };
-        controller.pcmd(maneuver);
-        controller.flush();
-    }, duration);
+async function move_right(duration, controller = control){
+    await repeat(
+        function() {
+            maneuver = { right: .5,
+                         front: -navdata_to_speed(pitch) };
+            controller.pcmd(maneuver);
+            controller.flush();
+        }, duration);
+    await stabilize(1500);
 }
+
+async function mission_engage() {
+    await takeoff();
+    await move_forward(2000);
+    await move_right(2000);
+    await move_backward(2000);
+    await move_left(2000);
+}
+
+mission_engage();
