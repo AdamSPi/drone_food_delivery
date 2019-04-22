6ab40f1b356a02d036f2132a81fff7a421096287
Adam SP <adamsp123@gmail.com>
2019-04-22 01:42:31 -0400

    i should go to bed

--- ar-drone.js
+++ ar-drone.js
@@ -1,2 +1,57 @@
-var arDrone = require('ar-drone');
-var client = arDrone.createClient();
+var df = require('dateformat')
+  , autonomy = require('ardrone-autonomy')
+  , mission  = autonomy.createMission()
+  , arDrone = require('ar-drone')
+  , arDroneConstants = require('./node_modules/ar-drone/lib/constants')
+  ;
+
+function navdata_option_mask(c) {
+  return 1 << c;
+}
+
+// From the SDK.
+var navdata_options = (
+    navdata_option_mask(arDroneConstants.options.DEMO)
+  | navdata_option_mask(arDroneConstants.options.VISION_DETECT)
+  | navdata_option_mask(arDroneConstants.options.MAGNETO)
+  | navdata_option_mask(arDroneConstants.options.WIFI)
+);
+
+// Land on ctrl-c
+var exiting = false;
+process.on('SIGINT', function() {
+    if (exiting) {
+        process.exit(0);
+    } else {
+        console.log('Got SIGINT. Landing, press Control-C again to force exit.');
+        exiting = true;
+        mission.control().disable();
+        mission.client().land(function() {
+            process.exit(0);
+        });
+    }
+});
+
+// Connect and configure the drone
+mission.client().config('general:navdata_demo', true);
+mission.client().config('general:navdata_options', navdata_options);
+mission.client().config('video:video_channel', 1);
+mission.client().config('detect:detect_type', 12);
+
+mission.log("mission-" + df(new Date(), "yyyy-mm-dd_hh-MM-ss") + ".txt");
+
+mission.takeoff()
+       .go({x:0, y:0, z:1})
+       .hover(30000)
+       .land();
+
+mission.run(function (err, result) {
+    if (err) {
+        console.trace("Oops, something bad happened: %s", err.message);
+        mission.client().stop();
+        mission.client().land();
+    } else {
+        console.log("We are done!");
+        process.exit(0);
+    }
+});
