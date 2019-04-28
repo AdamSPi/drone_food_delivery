3a140647b230653653025af85f2cac8290edf514
Adam SP <adamsp123@gmail.com>
2019-04-28 19:54:08 -0400

    fixed flask server on roompi

--- roomba_server.py
+++ roomba_server.py
@@ -2,9 +2,11 @@ from flask import Flask, request, json
 import subprocess
 import requests
 
+app = Flask(__name__)
+
 @app.route('/start')
 def start():
     # Raspberry pi told us to start
-	subprocess.Popen(['python', 'roomba_control.js'])
+    subprocess.Popen(['python', 'roomba_control.js'])
     print('Got start request from raspberrypi')
     return "Success"
