f8d920b92d60e247bf1c791c8a4edc551d5b17fc
Adam SP <adamsp123@gmail.com>
2019-04-28 19:16:32 -0400

    controls through usb not wifi :(

--- roomba.py
+++ roomba.py
@@ -1,59 +1,51 @@
-from  pycreate2 import Create2
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# MIT License
+# moves the roomba through a simple sequence
+
+from __future__ import print_function
+import pycreate2
 import time
 
-# Create a Create2.
-bot = Create2()
 
-# Start the Create 2
-bot.start()
+if __name__ == "__main__":
+    # Create a Create2 Bot
+	# port = '/dev/tty.usbserial-DA01NX3Z'  this is the serial port on my iMac
+	port = '/dev/ttyUSB0'  # this is the serial port on my raspberry pi
+	baud = {
+		'default': 115200,
+		'alt': 19200  # shouldn't need this unless you accidentally set it to this
+	}
 
-# Put the Create2 into 'safe' mode so we can drive it
-# This will still provide some protection
-bot.safe()
+	bot = pycreate2.Create2(port=port, baud=baud['default'])
 
-# You are responsible for handling issues, no protection/safety in
-# this mode ... becareful
-bot.full()
+	# define a movement path
+	path = [
+		['forward', 200, 1, 'for'],
+		['back', -200, 2, 'back'],
+		['stop', 0, 0.1, 'stop'],
+		['turn right', 100, 2, 'rite'],
+		['turn left', -100, 4, 'left'],
+		['turn right', 100, 2, 'rite'],
+		['stop', 0, 0.1, 'stop']
+	]
 
-# directly set the motor speeds ... easier if using a joystick
-bot.drive_direct(100, 100)
+	bot.start()
+	bot.safe()
 
-# turn an angle [degrees] at a speed: 45 deg, 100 mm/sec
-bot.turn_angle(45, 100)
+	# path to move
+	for mov in path:
+		name, vel, dt, string = mov
+		print(name)
+		bot.digit_led_ascii(string)
+		if name in ['forward', 'back', 'stop']:
+			bot.drive_straight(vel)
+			time.sleep(dt)
+		elif name in ['turn right', 'turn left']:
+			bot.drive_turn(vel, -1)
+		else:
+			raise Exception('invalid movement command')
 
-# drive straight for a distance: 5 meters, reverse 100 mm/sec
-drive_distance(5, -100)
-
-# Tell the Create2 to drive straight forward at a speed of 100 mm/s
-bot.drive_straight(100)
-time.sleep(2)
-
-# Tell the Create2 to drive straight backward at a speed of 100 mm/s
-bot.drive_straight(-100)
-time.sleep(2)
-
-# Turn in place
-bot.drive_turn(100, 0)
-time.sleep(2)
-
-# Turn in place
-bot.drive_turn(-100, 0)
-time.sleep(4)
-
-# Turn in place
-bot.drive_turn(100, 0)
-time.sleep(2)
-
-# use the simpler drive direct
-bot.drive_direct(200,-200)  # inputs for motors are +/- 500 max
-time.sleep(2)
-
-# Stop the bot
-bot.drive_stop()
-
-# query some sensors
-sensors = bot.get_sensors()  # returns all data
-print(sensors.light_bumper_left)
-
-# Close the connection
-# bot.close()
+	print('shutting down ... bye')
+	bot.drive_stop()
+	time.sleep(0.1)
