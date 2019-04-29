9b03dd7f37d59cbd07364f96b5c0a2d3d78f053e
Adam SP <adamsp123@gmail.com>
2019-04-28 20:14:04 -0400

    testing drone more

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def DroneIntent(status, room):
 		# make a request to http://192.168.1.3:5000/start to start roomba
 		# requests.get('http://192.168.1.3:5000/start')
 		# start node program to hover drone over tag
-		subprocess.Popen(['node', 'hover_over_tag.js'])
+		subprocess.Popen(['node', 'manual_drone_hover.js'])
 		return statement('{}ing drone.'.format(status))
 	elif status in STATUSOFF:
 		print('Landing drone...')
